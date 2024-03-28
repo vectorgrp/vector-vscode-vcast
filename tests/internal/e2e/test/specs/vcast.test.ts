fde26834f1efb775c5995072b8caf3b693d16f7e
Igor Martinovic <igor.martinovic@vector.com>
2024-03-28 14:06:16 +0100

    Checking that the comment remains in the debug config file after processing

--- tests/internal/e2e/test/specs/vcast.test.ts
+++ tests/internal/e2e/test/specs/vcast.test.ts
@@ -1342,7 +1342,7 @@ describe("vTypeCheck VS Code Extension", () => {
     await (await $("aria/VectorCAST: Add Launch Configuration")).click();
 
     console.log("Validating that debug launch configuration got generated");
-    const debugConfigTab = (await editorView.openEditor(
+    let debugConfigTab = (await editorView.openEditor(
       "launch.json",
     )) as TextEditor;
     
@@ -1410,7 +1410,13 @@ describe("vTypeCheck VS Code Extension", () => {
     const activeTabTitle = await activeTab.getTitle();
     console.log(activeTabTitle);
     expect(activeTabTitle).toBe("manager_inst.cpp");
-
+    
+    // checking that the debug config file still has the comment we added
+    debugConfigTab = (await editorView.openEditor(
+      "launch.json",
+    )) as TextEditor;
+    const commentLine = await debugConfigTab.getTextAtLine(2)
+    expect(commentLine).toBe("// This is a comment")
     console.log("Finished creating debug configuration");
   });
 
