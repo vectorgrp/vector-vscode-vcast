d76e090b1124d4583d983514488c1d56fd14eb27
SalvatoreZagaria <salvatore.zagaria@vector.com>
2024-03-29 13:18:15 +0000

    SZ: Fixing proxy issues

--- tests/internal/e2e/run_e2e_tests.sh
+++ tests/internal/e2e/run_e2e_tests.sh
@@ -8,6 +8,15 @@ activate_24_release () {
   echo "Vcast 24 is activated"
 }
 
+set_forward_proxy () {
+  echo "Setting forward proxy..."
+
+  sudo /usr/sbin/squid
+
+  NEW_PROXY="http://$(hostname --ip-address):3128"
+  export HTTP_PROXY=$NEW_PROXY HTTPS_PROXY=$NEW_PROXY http_proxy=$NEW_PROXY https_proxy=$NEW_PROXY GLOBAL_AGENT_HTTP_PROXY=$NEW_PROXY GLOBAL_AGENT_HTTP_PROXY=$NEW_PROXY
+}
+
 cd $ROOT
 if [ ! -d "node_modules" ]; then
   npm install
@@ -21,6 +30,7 @@ if [ "$GITHUB_ACTIONS" = "true" ] || [ "$TESTING_IN_CONTAINER" = "True" ] ; then
     if [ "$GITHUB_ACTIONS" = "true" ] ; then
       source /home/vcast_user/.bashrc
     fi
+    set_forward_proxy
     if [ "$(pidof /usr/bin/Xvfb)" == "" ]; then
         echo "Starting xvfb..."
         Xvfb :99 -screen 0 1920x1080x24 &
