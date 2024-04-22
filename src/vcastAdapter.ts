bf92ee3164314a9751bef7fcebe32acf4c9fb68e
John Paliotta <john.paliotta@vector.com>
2024-04-22 07:48:29 -0400

    Turn off server

--- src/vcastAdapter.ts
+++ src/vcastAdapter.ts
@@ -51,7 +51,7 @@ import {
 
 // This is the core extension version of the flag, set on
 // initialization or when the setting value is changed.
-export let globalEnviroServerActive: boolean = true;
+export let globalEnviroServerActive: boolean = false;
 
 const path = require("path");
 
