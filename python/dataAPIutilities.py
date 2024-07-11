ddbe4ffe9c8027e96297af593b95d797acd7881c
Andrew V. Teylu <andrew.teylu@vector.com>
2024-07-11 22:54:45 +0100

    Remove the condition on mock_lookup_type

--- python/dataAPIutilities.py
+++ python/dataAPIutilities.py
@@ -131,13 +131,9 @@ def functionCanBeMocked(functionObject):
     # Destructors are not supported by vmock
     elif "~" in functionObject.vcast_name:
         return False
+    # This allows us to support older versions of VectorCAST
     elif hasattr(functionObject, "is_mockable"):
-        # FIXME: this is a hack to avoid generating applys that don't have lookups
-        # Andrew: Which example that causes this issue?
-        if not functionObject.mock_lookup_type:
-            return False
-        else:
-            return functionObject.is_mockable
+        return functionObject.is_mockable
     else:
         return True
 
