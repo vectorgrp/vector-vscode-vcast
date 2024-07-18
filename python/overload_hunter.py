f5d3b3edbbd1928a4bcb184ee2b40a248b7ca12b
Andrew V. Teylu <andrew.teylu@vector.com>
2024-07-18 15:41:15 -0400

    Overload hunter

--- python/overload_hunter.py
+++ python/overload_hunter.py
@@ -9,7 +9,7 @@ def main():
     api = UnitTestApi(sys.argv[1])
 
     count = 0
-    with open("tests.cpp", "w") as fd:
+    with open(sys.argv[2], "w") as fd:
         fd.write('#include "unit.cpp"\n')
         fd.write("void blahblahblahblah(void) {\n")
         for unit in api.Unit.all():
@@ -17,6 +17,9 @@ def main():
                 if not functionCanBeMocked(function):
                     continue
                 if function.is_overloaded:
+                    continue
+                name_for_addr = getFunctionNameForAddress(api, function)
+                if "vcast_concrete_" in name_for_addr or name_for_addr in ["pthread_cond_clockwait", "pthread_mutex_clocklock"] or "(" in name_for_addr:
                     continue
                 count += 1
                 fd.write(
