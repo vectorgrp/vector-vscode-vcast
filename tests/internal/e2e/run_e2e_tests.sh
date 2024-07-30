88a38146bbed62fa81b33667b1696e7c80aac7ef
Den1552 <44575706+Den1552@users.noreply.github.com>
2024-07-30 13:23:46 +0200

    Add test cases if  $VECTORCAST_DIR or $PATH are not set. (#158)
    This PR adds tests in case `$VECTORCAST_DIR` is not set, `clicast` isn't
    on `$PATH`
    
    **Changes**:
    
    - Adds new spec group `build_env_failure` with 2 specs
    `vcast_build_env_failure.test.ts` and
      `vcast_build_env_after_failure.test.ts`
    - Modifies `run_e2e_tests.sh` to set `$VECTORCAST_DIR` and `$PATH` for
    testing
    - Modifies `specs_config.ts`, including possible env variables and
    parameters for each spec group
    - Modifies `wdio.conf.ts` in order to not execute `clicast` when
    `$VECTORCAST_DIR` or `$PATH` are not correct
    
    Closes #60

--- tests/internal/e2e/run_e2e_tests.sh
+++ tests/internal/e2e/run_e2e_tests.sh
@@ -1,12 +1,70 @@
 #!/bin/bash
 ROOT=$(dirname "$(realpath "$0")")
 
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$0")
+
+# Define the path to the specs file relative to the script directory
+SPEC_PATH="$SCRIPT_DIR/test/specs_env_exporter.ts"
+
+
+# Check if the specs file exists
+if [ ! -f "$SPEC_PATH" ]; then
+  echo "The file $SPEC_PATH does not exist."
+  exit 1
+fi
+
+# Compile the specs file 
+npx tsc "$SPEC_PATH"
+
+# Path to the compiled JavaScript files
+JS_FILE="./test/specs_env_exporter.js"
+SPECS_CONFIG_FILE="./test/specs_config.js"
+
 activate_24_release () {
   export VECTORCAST_DIR=/vcast/release24
   export PATH=/vcast/release24:$PATH
   export ENABLE_ATG_FEATURE=TRUE
   echo "Vcast 24 is activated"
 }
+
+set_specs_params() {
+  # Check if RUN_GROUP_NAME is set
+  if [ -z "$RUN_GROUP_NAME" ]; then
+    echo "RUN_GROUP_NAME is not set. Please set it and try again."
+    exit 1
+  fi
+
+  #Check if the compiled JavaScript file exists
+  if [ ! -f "$JS_FILE" ]; then
+    echo "Compiled specs_env_exporter.js file not found!"
+    exit 1
+  fi
+
+  #Check if the compiled JavaScript file exists
+  if [ ! -f "$SPECS_CONFIG_FILE" ]; then
+    echo "Compiled specs_config.js file not found!"
+    exit 1
+  fi
+
+  # Extract environment variables for the given group using the compiled JavaScript file
+  env_vars=$(node $JS_FILE)
+
+  # Check if env_vars is empty, indicating either the group or env section might be missing
+
+  # Export every env from the group
+  for env_var_val in $env_vars; do
+    export "$env_var_val"
+  done
+
+  # Print the values to verify exported env variables
+  echo "Environment variables for $RUN_GROUP_NAME have been set:"
+  while IFS= read -r line; do
+    echo "$line"
+  done <<< "$env_vars"
+
+}
+
 
 cd $ROOT
 if [ ! -d "node_modules" ]; then
@@ -20,6 +78,7 @@ fi
 if [ "$GITHUB_ACTIONS" = "true" ] ; then
   source /home/vcast_user/.bashrc
 fi
+set_specs_params
 if [ "$GITHUB_ACTIONS" = "true" ] || [ "$TESTING_IN_CONTAINER" = "True" ] ; then
     if [ "$(pidof /usr/bin/Xvfb)" == "" ]; then
         echo "Starting xvfb..."
@@ -43,4 +102,6 @@ if [ "$GITHUB_ACTIONS" = "true" ] || [ "$TESTING_IN_CONTAINER" = "True" ] ; then
     fi
 else
     npx wdio run test/wdio.conf.ts
+    rm "$JS_FILE" 
+    rm "$SPECS_CONFIG_FILE"
 fi
