ab179c2473ea200909d1f7c9ed5d0688b338cc34
John Paliotta <john.paliotta@vector.com>
2024-08-12 15:45:39 -0400

    Migrated server and associated tests to the extension

---
+++ python/vcastDataServer.py
@@ -0,0 +1,211 @@
+import argparse
+import copy
+import json
+import os
+import sys
+import signal
+import traceback
+
+import vcastDataServerTypes
+from vcastDataServerTypes import commandType
+
+
+from vector.apps.DataAPI.unit_test_api import UnitTestApi as Api
+
+# flask was added to vpthon for vc24sp4
+from flask import Flask, request
+
+app = Flask(__name__)
+
+import clicastInterface
+import testEditorInterface
+import tstUtilities
+import vTestInterface
+import vTestUtilities
+from vTestUtilities import logMessage
+
+
+@app.route("/ping")
+def ping():
+    logMessage("vcastDataServer: received ping request, responding 'alive'")
+    return {"text": "alive"}
+
+
+@app.route("/shutdown")
+def shutdown():
+    logMessage("vcastDataServer: received shutdown request ...")
+    # terminate all of the clicast processes
+    # Need a copy of the keys because we are modifying the dictionary
+    # whild looping over it would cause an error
+    keyList = list(clicastInterface.clicastInstances.keys())
+    for enviroPath in keyList:
+        logMessage(f"  terminating clicast process for: {enviroPath}")
+        clicastInterface.terminateClicastProcess(enviroPath)
+
+    # TBD: is there an app.shutdown() call to do this?
+    logMessage("  vcastDataServer is exiting ...")
+    sys.exit(0)
+
+
+@app.route("/vassistant")
+def vassistant():
+    """
+    This function does the work of translating the clientRequest object into a call
+    to the vTestInterface module and then sending the response object back to the client
+    """
+
+    # Is it possible to receive a python class directly
+    clientRequestText = request.args.get("request")
+    clientRequest = decodeRequest(clientRequestText)
+
+    try:
+        logMessage(
+            f"\nvcastDataServer: received client request: {clientRequest.command} for {clientRequest.path}"
+        )
+        logMessage(f"  clicastInstances: {clicastInterface.clicastInstances.keys()}")
+        exitCode = 0
+        if clientRequest.command == commandType.closeConnection:
+
+            returnValue = clicastInterface.terminateClicastProcess(clientRequest.path)
+            returnData = {
+                "status": returnValue,
+                "newlist": list(clicastInterface.clicastInstances.keys()),
+            }
+            logMessage(
+                f"  clicastInstances: {clicastInterface.clicastInstances.keys()}"
+            )
+
+        elif clientRequest.command == commandType.choiceList:
+
+            # because we are not restarting, we need to clear the global output log
+            testEditorInterface.globalOutputLog.clear()
+
+            choiceData = testEditorInterface.processLine(
+                clientRequest.path, clientRequest.options
+            )
+            returnData = tstUtilities.buildChoiceResponse(choiceData)
+            logMessage(f"  line received: '{clientRequest.options}'")
+            logMessage(
+                "  list returned:\n     " + "\n     ".join(returnData["choiceList"])
+            )
+
+        elif clientRequest.command == commandType.runClicastCommand:
+
+            exitCode, returnData = clicastInterface.runClicastServerCommand(
+                clientRequest.path, clientRequest.options
+            )
+
+        elif clientRequest.command in vTestInterface.modeChoices:
+
+            exitCode, returnData = vTestInterface.processCommand(
+                clientRequest.command,
+                clientRequest.clicast,
+                clientRequest.path,
+                clientRequest.test,
+                clientRequest.options,
+            )
+
+        elif clientRequest.command == "crash-me":
+            # this is for unit testing only to force a stack trace
+            raise Exception("crash-me command received")
+
+        elif clientRequest.command == "bad-request-format":
+            errorMessage = f"client request was improperly formatted, ignoring: '{clientRequestText}'"
+            logMessage(f"  ERROR: {errorMessage}")
+            exitCode = 999
+            returnData = {"error": [errorMessage]}
+
+        else:
+            errorMessage = f"server does not support command: '{clientRequest.command}'"
+            logMessage(f"  ERROR: {errorMessage}")
+            exitCode = 999  # internal server error
+            returnData = {"error": [errorMessage]}
+
+    except Exception as error:
+        # if anything goes wrong send the stack trace back to the client
+        errorMessage = "internal server error"
+        logMessage(f"  ERROR: {errorMessage}")
+        errorTextToReturn = [errorMessage]
+        errorTextToReturn.extend(traceback.format_exc().split("\n"))
+        exitCode = 999  # internal server error
+        returnData = {"error": errorTextToReturn}
+
+    return {"exitCode": exitCode, "data": returnData}
+
+
+def decodeRequest(requestString):
+
+    clientRequest = None
+    try:
+        requestDictionary = json.loads(requestString)
+        clientRequest = vcastDataServerTypes.clientRequest.fromDict(requestDictionary)
+    except KeyboardInterrupt:
+        raise
+    except:
+        clientRequest = vcastDataServerTypes.clientRequest("bad-request-format")
+
+    return clientRequest
+
+
+def setupArgs():
+    parser = argparse.ArgumentParser(description="VectorCAST Data Server")
+    parser.add_argument(
+        "--port", help=f"Server port number (default={vcastDataServerTypes.PORT})"
+    )
+    return parser
+
+
+def serverSignalHandler(signum, frame):
+    logMessage(f"Server caught signal {signum}")
+    shutdown()
+
+
+def main():
+    """
+    This is the enviro data server that allows the VS Code Test Explorer
+    to interact with the VectorCAST environment.
+    """
+
+    # force server mode on
+    clicastInterface.USE_SERVER = True
+
+    # set the global clicast command
+    # we are running under vpython so we use that to find the path to clicast
+    vcastInstallation = os.path.dirname(sys.executable)
+    clicastInterface.globalClicastCommand = os.path.join(vcastInstallation, "clicast")
+
+    argParser = setupArgs()
+    args, _ = argParser.parse_known_args()
+
+    # process port arg if it exists
+    vcastDataServerTypes.processPortArg(args)
+
+    # By registering this signal hander we can
+    # allow ctrl-c to shutdown the server gracefully
+    signal.signal(signal.SIGTERM, serverSignalHandler)
+    signal.signal(signal.SIGINT, serverSignalHandler)
+    if sys.platform == "win32":
+        signal.signal(signal.SIGBREAK, serverSignalHandler)
+
+    # start the server
+    with open("vcastDataServer.log", "w", buffering=1) as vTestUtilities.logFileHandle:
+        print(
+            f" * vcastDataServer is starting on {vcastDataServerTypes.HOST}:{vcastDataServerTypes.PORT} ..."
+        )
+        app.run(vcastDataServerTypes.HOST, vcastDataServerTypes.PORT, threaded=False)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+        sys.exit(0)
+
+    # allow ctr-c to stop program without messages
+    except KeyboardInterrupt:
+        shutdown()
+
+    except Exception as err:
+        print(Exception, err)
+        print(traceback.format_exc())
+        print(" ")
+        sys.exit(1)
--- tests/README.md
+++ tests/README.md
@@ -1,3 +1,10 @@
 - To run the vitest-based unit tests, run `npm test` in the root of the repository.
-- Coverage info is found under `coverage/index.html` in the root of the repository.
-- VectorCAST test environment which is created for running the unit tests is `tests/unit/vcast`
+  - Coverage info is found under `coverage/index.html` in the root of the repository.
+  - VectorCAST test environment which is created for running the unit tests is `tests/unit/vcast`
+
+
+- To run the vcast data sever tests, proceed as follows
+  - Set VECTORCAST_DIR - Path to the installation vc24sp4+
+  - Start the server using something like: vpython c:\rds\vector-vscode-vcast\python\vcastDataServer.py
+  - Run the client using something like: vpython c:\rds\vector-vscode-vcast\tests\clicast-server\client.py
+
