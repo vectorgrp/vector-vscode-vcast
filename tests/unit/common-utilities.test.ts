6cfead09a7e8e328ad4fbbddeedb8cb820d4b98c
John Paliotta <john.paliotta@vector.com>
2024-08-29 10:53:12 -0400

    Refactored the clean*output processing to be a single function for all cases rather than one for vpython and one for clicast

--- tests/unit/common-utilities.test.ts
+++ tests/unit/common-utilities.test.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import process from "node:process";
 import { describe, expect, test } from "vitest";
 import {
-  cleanVPythonOutput,
+  cleanVectorcastOutput,
   getVcastOptionValues,
 } from "../../src-common/commonUtilities";
 
@@ -33,14 +33,14 @@ describe("Validating commonUtilities", () => {
     timeout
   );
   test(
-    "validate cleanVPythonOutput",
+    "validate cleanVectorcastOutput",
     async () => {
       let testString =
         "some stuff to be stripped\n\n  ACTUAL-DATA\n   some more stuff ";
-      expect(cleanVPythonOutput(testString)).toBe("some more stuff");
+      expect(cleanVectorcastOutput(testString)).toBe("some more stuff");
 
       testString = "don't strip me some more stuff";
-      expect(cleanVPythonOutput(testString)).toBe(testString);
+      expect(cleanVectorcastOutput(testString)).toBe(testString);
     },
     timeout
   );
