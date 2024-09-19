96e6b79ef8bec13ce8a3ab665ceca87233efff05
Denis Moslavac <Denis.Moslavac@vector.com>
2024-09-19 16:54:47 +0200

    Changed log for test.

--- tests/internal/e2e/test/specs/vcast_testdel_func_basis.test.ts
+++ tests/internal/e2e/test/specs/vcast_testdel_func_basis.test.ts
@@ -96,17 +96,17 @@ describe("vTypeCheck VS Code Extension", () => {
 
     await browser.waitUntil(
       async () => (await outputView.getText()).at(-1) != undefined,
-      { timeout: 30_000, interval: 1000 }
+      { timeout: TIMEOUT, interval: 1000 }
     );
 
     await browser.waitUntil(
       async () =>
         (await outputView.getText())
-          .at(-1)
           .toString()
-          .includes("Processing environment data for:"),
-      { timeout: 30_000, interval: 1000 }
+          .includes("Processing environment data"),
+      { timeout: TIMEOUT }
     );
+
     await browser.pause(10_000);
 
     await assertTestsDeleted("DATABASE-MANAGER");
