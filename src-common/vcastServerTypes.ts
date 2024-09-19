c087afbd21c2704a98b0b324e6061be7ec90feb1
Denis Moslavac <Denis.Moslavac@vector.com>
2024-09-19 09:05:09 +0200

    Changed pythonError codes to be less than 255

--- src-common/vcastServerTypes.ts
+++ src-common/vcastServerTypes.ts
@@ -1,9 +1,6 @@
-
-
 export enum pythonErrorCodes {
-    internalServerError = 999,
-    testInterfaceError = 998,
-    couldNotStartClicastInstance = 997,
-    codedTestCompileError = 996
+  internalServerError = 99,
+  testInterfaceError = 98,
+  couldNotStartClicastInstance = 97,
+  codedTestCompileError = 96,
 }
-
