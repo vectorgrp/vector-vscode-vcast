ec83e41fa852e192be434645d42972bf7173c656
John Paliotta <john.paliotta@vector.com>
2024-09-20 14:35:45 -0400

    Moved constants back to global scope as they are used in two functions, client.py test caught this

--- python/clicastInterface.py
+++ python/clicastInterface.py
@@ -195,14 +195,15 @@ def runClicastScript(enviroPath, commandFileName, echoToStdout=False):
         return runClicastScriptCommandLine(commandFileName, echoToStdout)
 
 
+tempEnviroScript = "rebuild.env"
+tempTestScript = "rebuild.tst"
+
+
 def updateScriptsAndRebuild(enviroPath, jsonOptions):
     """
     This does the actual work of updating the scripts
     and invoking the build and load test script commands
     """
-
-    tempEnviroScript = "rebuild.env"
-    tempTestScript = "rebuild.tst"
 
     enviroName = os.path.basename(enviroPath)
 
