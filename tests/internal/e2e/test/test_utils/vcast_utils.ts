b64223e6d3d76c2af7b3a53d45ff65ef6207f21a
Denis Moslavac <Denis.Moslavac@vector.com>
2024-09-20 16:16:49 +0200

    Added comment for TIMEOUT.

--- tests/internal/e2e/test/test_utils/vcast_utils.ts
+++ tests/internal/e2e/test/test_utils/vcast_utils.ts
@@ -15,6 +15,7 @@ import { Key } from "webdriverio";
 import expectedBasisPathTests from "../basis_path_tests.json";
 import expectedAtgTests from "../atg_tests.json";
 
+// Local VM takes longer and needs a higher TIMEOUT
 export const TIMEOUT = 180_000;
 
 const promisifiedExec = promisify(exec);
