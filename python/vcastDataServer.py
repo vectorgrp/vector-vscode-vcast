9f52fdef52c15c9bcc9eec3ed639e016be33c9ee
Denis Moslavac <Denis.Moslavac@vector.com>
2024-10-15 17:08:05 +0200

    Added flush in print.

--- python/vcastDataServer.py
+++ python/vcastDataServer.py
@@ -45,9 +45,9 @@ def init_application(logFilePath):
         # Note: this string must match what is in vcastAdapter.ts -> startServer()
         # We prefix the string with " * " so that it matches the flask output
         print(
-            f" * vcastDataServer is starting on {vcastDataServerTypes.HOST}:{vcastDataServerTypes.PORT}"
+            f" * vcastDataServer is starting on {vcastDataServerTypes.HOST}:{vcastDataServerTypes.PORT}", flush=True
         )
-        print(f" * Server log file path: {logFilePath}")
+        print(f" * Server log file path: {logFilePath}", flush=True)
         logMessage(
             f"{logPrefix()} port: {vcastDataServerTypes.PORT} clicast: {pythonUtilities.globalClicastCommand}\n"
         )
