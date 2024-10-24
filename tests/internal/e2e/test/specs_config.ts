80267534c346b49a0cf12cebe287fe52d242e1cf
Denis Moslavac <Denis.Moslavac@vector.com>
2024-10-24 17:37:40 +0200

    Adapted function because release naming pattern changed.

--- tests/internal/e2e/test/specs_config.ts
+++ tests/internal/e2e/test/specs_config.ts
@@ -210,8 +210,8 @@ export function getSpecs(vcast24: boolean, group: string = null) {
 }
 
 /**
- * Splits all paths from the PATH env variable that contain a "release".
- * @returns New PATH env var without "release" paths
+ * Splits all paths from the PATH env variable that contain a year followed by "sp" and a number (e.g., 2023sp2).
+ * @returns New PATH env var without those paths
  */
 function removeReleaseOnPath(): string | undefined {
   // Get the PATH environment variable
@@ -225,8 +225,11 @@ function removeReleaseOnPath(): string | undefined {
   // Split the PATH on ":"
   const paths = envPath.split(":");
 
-  // Filter out paths that contain "release"
-  const filteredPaths = paths.filter((path) => !path.includes("release"));
+  // Regex to match paths that start with a year followed by "sp" and a number (e.g., 2023sp0)
+  const releaseRegex = /^\d{4}sp\d+/;
+
+  // Filter out paths that match the new release pattern
+  const filteredPaths = paths.filter((path) => !releaseRegex.test(path));
 
   // Join the remaining paths back together with ":"
   const newPath = filteredPaths.join(":");
