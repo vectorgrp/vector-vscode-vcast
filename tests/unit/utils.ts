bc8697cbac81d491ff61f9f128f2937226cf0d43
Denis Moslavac <Denis.Moslavac@vector.com>
2024-10-25 08:57:41 +0200

    Changed getToolVersion again.

--- tests/unit/utils.ts
+++ tests/unit/utils.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import process from "node:process";
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
+import fs from "node:fs";
 import {
   TextDocument,
   TextDocuments,
@@ -295,3 +296,74 @@ export const setupDiagnosticTest = (diagnostic: Diagnostic) => {
 
   return { connection, mockSendDiagnostics };
 };
+
+const promisifiedExec = promisify(exec);
+
+/**
+ * Function to get the clicast executable path and check the tool version
+ */
+export async function getToolVersion() {
+  // Determine the command to locate clicast
+  const checkClicast =
+    process.platform === "win32" ? "where clicast" : "which clicast";
+
+  let clicastExecutablePath = "";
+
+  try {
+    // Execute the command to find clicast
+    const { stdout, stderr } = await promisifiedExec(checkClicast);
+    if (stderr) {
+      throw new Error(
+        `Error when running ${checkClicast}, make sure clicast is on PATH`
+      );
+    } else {
+      clicastExecutablePath = stdout.trim();
+      console.log(`clicast found in ${clicastExecutablePath}`);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error: ${error.message}`);
+    } else {
+      console.error(`Unexpected error: ${String(error)}`);
+    }
+
+    throw new Error(
+      `Error when running "${checkClicast}", make sure clicast is on PATH`
+    );
+  }
+
+  // Read the tool version from the appropriate path
+  const toolVersionPath = path.join(
+    clicastExecutablePath,
+    "..",
+    "DATA",
+    "tool_version.txt"
+  );
+
+  try {
+    const toolVersion: string = fs
+      .readFileSync(toolVersionPath)
+      .toString()
+      .trim();
+
+    // Extract the first two characters & try to cast it to a number
+    const firstTwoChars = toolVersion.slice(0, 2);
+    const versionNumber = Number(firstTwoChars);
+
+    // Check if the conversion was successful (not NaN)
+    if (!isNaN(versionNumber)) {
+      return versionNumber;
+    } else {
+      console.error(`Error: Could not cast "${firstTwoChars}" to a number`);
+      return NaN;
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error reading tool version: ${error.message}`);
+    } else {
+      console.error(`Unexpected error: ${String(error)}`);
+    }
+
+    return NaN;
+  }
+}
