298108e5025bf6171a0427186332aaed7e7f4916
Patrick Bareiss <Patrick.Bareiss@vector.com>
2025-01-24 13:21:47 +0100

    Return partial tests earlier by default

--- autoreq/test_generation/generation.py
+++ autoreq/test_generation/generation.py
@@ -171,7 +171,7 @@ class TestGenerator:
             self.info_logger.increment_retries_used(requirement_id)
             temperature = 0.0 if first_try else 1.0
             extended_reasoning = self.use_extended_reasoning and not first_try  # Modify this line
-            allow_partial = not first_try
+            allow_partial = True#not first_try
             result = await self._generate_test_case_no_retries(requirement_id, temperature=temperature, extended_reasoning=extended_reasoning, allow_partial=allow_partial)
             if result:
                 self.info_logger.set_test_generated(requirement_id)
@@ -278,7 +278,7 @@ Notes:
             return None
 
         test_generation_result = await self._iterative_error_correction(
-            requirement_id, test_generation_result, messages, schema, temperature=temperature, extended_reasoning=extended_reasoning, allow_partial=allow_partial)
+            requirement_id, test_generation_result, messages, schema, temperature=temperature, extended_reasoning=extended_reasoning, allow_partial=allow_partial, allow_early_partial=True)
 
         if test_generation_result is None:
             return None
@@ -297,7 +297,7 @@ Notes:
         
         return partial_result
 
-    async def _iterative_error_correction(self, requirement_id, test_generation_result, messages, schema, temperature=0.0, extended_reasoning=False, max_iterations=3, allow_partial=False):
+    async def _iterative_error_correction(self, requirement_id, test_generation_result, messages, schema, temperature=0.0, extended_reasoning=False, max_iterations=3, allow_partial=False, allow_early_partial=False):
         iteration = 0
         fix_messages = messages
         while iteration < max_iterations:
@@ -310,6 +310,9 @@ Notes:
 
             if not errors and not test_failures:
                 break
+
+            if not errors and allow_partial and allow_early_partial:
+                return self._create_partial_test_case(test_generation_result)
 
             # Set test_run_failure_feedback only if test failures are detected
             if test_failures:
