ab38193ac5039592ca1fd492cf33a2284ca391f5
SalvatoreZagaria <salvatore.zagaria@vector.com>
2025-03-13 11:59:06 +0000

    Simplify conditional checks for environment setup.
    Removed unnecessary conditional logic for piinnovo and atg-customer environments, ensuring that both source URLs are processed unconditionally.

--- ci/reqs2tests_eval.sh
+++ ci/reqs2tests_eval.sh
@@ -48,18 +48,12 @@ setup() {
   mkdir $VCAST_USER_HOME/.envs
   mkdir $VCAST_USER_HOME/.src
 
-  if [ "$ENV_SET_NAME" == "piinovo" ] || [ "$ENV_SET_NAME" == "atg-customer" ]; then
-    # shellcheck disable=SC2164
-    cd $VCAST_USER_HOME/.src
-    process_url $PIINNOVO_SRC
-    export PI_INNOVO_SRC_PATH=$PWD/piinnovo-source
-    if [ "$ENV_SET_NAME" == "atg-customer" ]; then
-      process_url $HALLA_SRC
-      export HALLA_MODMGR4A_SRC_PATH=$PWD/halla-modmgr4a-source
-    fi
-  fi
+  cd $VCAST_USER_HOME/.src
+  process_url $PIINNOVO_SRC
+  export PI_INNOVO_SRC_PATH=$PWD/piinnovo-source
+  process_url $HALLA_SRC
+  export HALLA_MODMGR4A_SRC_PATH=$PWD/halla-modmgr4a-source
 
-  # shellcheck disable=SC2164
   cd $VCAST_USER_HOME/.envs
   process_url $ENV_SET_URL
 }
