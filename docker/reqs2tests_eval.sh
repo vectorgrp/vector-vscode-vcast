07acca60620852b969170e6bdf644cbeeb68d72e
SalvatoreZagaria <salvatore.zagaria@vector.com>
2025-03-13 10:46:24 +0000

    Add MAX_COST parameter and refactor environment handling
    Introduced a MAX_COST input parameter to control test cost limits. Refactored environment selection logic in both the workflow and script, replacing hardcoded matrix values with dynamic input handling. This improves flexibility and enables better control over test configurations.

--- docker/reqs2tests_eval.sh
+++ docker/reqs2tests_eval.sh
@@ -2,6 +2,34 @@
 
 export PATH=$VECTORCAST_DIR:$PATH
 
+SANITY_ENVS="https://rds-vtc-docker-dev-local.vegistry.vg.vector.int/artifactory/rds-build-packages-generic-dev-local/code2reqs2tests/sanity.tar.gz"
+PIINNOVO_ENVS="https://rds-vtc-docker-dev-local.vegistry.vg.vector.int/artifactory/rds-build-packages-generic-dev-local/code2reqs2tests/piinnovo-real-reqs.tar.gz"
+HALLA_ENVS="https://rds-vtc-docker-dev-local.vegistry.vg.vector.int/artifactory/rds-build-packages-generic-dev-local/code2reqs2tests/atg-customer.tar.gz"
+
+ENVS=("sanity" "piinnovo" "atg-customer")
+if [[ "$ENV_SET_NAME" == "sanity" ]]; then
+  ENV_SET_URL=$SANITY_ENVS
+elif [[ "$ENV_SET_NAME" == "piinnovo" ]]; then
+  ENV_SET_URL=$PIINNOVO_ENVS
+elif [[ "$ENV_SET_NAME" == "atg-customer" ]]; then
+  ENV_SET_URL=$HALLA_ENVS
+fi
+
+if [[ -z "$ENV_SET_URL" ]]; then
+  echo "Error: ENV_SET_NAME must be set to one of the following: ${ENVS[*]}"
+  exit 1
+fi
+echo "Running evaluation for $ENV_SET_NAME"
+
+MAX_COST_CHECK=$(echo "$MAX_COST" | python3.10 -c "import sys;exec('try:obj=float(str(sys.stdin.read().strip()))\nexcept:obj=-1');print(obj > 0)")
+if [[ "$MAX_COST_CHECK" == "False" ]]; then
+  MAX_COST_STR=""
+  echo "No max-cost set"
+else
+  echo "Max cost set to $MAX_COST"
+  MAX_COST_STR="--max-cost $MAX_COST"
+fi
+
 process_url() {
   local url="$1"
   local downloaded_file
@@ -37,7 +65,7 @@ main () {
 
   find . -iname '*.env' > bench_envs.txt
   source /.venv/bin/activate
-  reqs2tests_eval @bench_envs.txt --batched --allow-partial --timeout 30 r2t_eval_results
+  reqs2tests_eval @bench_envs.txt --batched --allow-partial --timeout 30 "$MAX_COST_STR" r2t_eval_results
   deactivate
   rm -rf /.envs /.src
 }
