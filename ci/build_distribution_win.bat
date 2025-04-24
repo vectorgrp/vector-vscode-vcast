17fa2e952c978e1561b824507a078470ff6e6540
SalvatoreZagaria <salvatore.zagaria@vector.com>
2025-04-24 14:21:51 +0100

    Bug fix

--- ci/build_distribution_win.bat
+++ ci/build_distribution_win.bat
@@ -5,7 +5,7 @@ cd dist
 move autoreq distribution
 mkdir distribution\_internal\monitors4codegen\multilspy\language_servers\clangd_language\
 cd distribution\_internal\monitors4codegen\multilspy\language_servers\clangd_language\
-curl -o clangd-linux-18.1.3.zip https://rds-vtc-docker-dev-local.vegistry.vg.vector.int:443/artifactory/rds-build-packages-generic-dev-local/code2reqs2tests/clangd/clangd-windows-18.1.3.zip
+curl -o clangd-windows-18.1.3.zip https://rds-vtc-docker-dev-local.vegistry.vg.vector.int:443/artifactory/rds-build-packages-generic-dev-local/code2reqs2tests/clangd/clangd-windows-18.1.3.zip
 powershell -command "Expand-Archive -Path clangd-windows-18.1.3.zip -DestinationPath ."
 mkdir clangd
 move clangd_18.1.3 clangd\
