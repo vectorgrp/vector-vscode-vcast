62a37dedb1b847ec77808cf5a7aa0b72ac89d776
Zagaria, Salvatore <Salvatore.Zagaria@vector.com>
2025-05-16 10:03:51 +0100

    Tests for reqs2tests (#7)
    Adding linux and windows workflows to pull and do minimal CLI-only deployment tests for the latest reqs2tests releases.

--- ci/get_most_recent_reqs2tests_distribution.py
+++ ci/get_most_recent_reqs2tests_distribution.py
@@ -1,17 +1,54 @@
 import os
 import json
 import logging
+import platform
 import tempfile
+from pathlib import Path
 from datetime import datetime
 
+DISTRIBUTION_NAMES = ("autoreq-linux.tar.gz", "autoreq-win.tar.gz")
+
+
+def download_file(url, filename=None):
+    if not filename:
+        filename = url.rstrip("/").split("/")[-1]
+
+    if platform.system() == "Windows":
+        cmd = f"(New-Object System.Net.WebClient).DownloadFile('{url}', '{filename}')"
+        cmd = f'powershell -Command "{cmd}" > $null 2>&1'
+    else:
+        cmd = f"wget {url} -O {filename}"
+        cmd += " > /dev/null 2>&1"
+    os.system(cmd)
+
+
+if os.getenv("R2T_RELEASE_URL_LIN"):
+    logging.info(
+        f"Using R2T_RELEASE_URL_LIN: {os.getenv('R2T_RELEASE_URL_LIN')} for Linux"
+    )
+    url = os.getenv("R2T_RELEASE_URL").rstrip("/")
+    download_file(url)
+if os.getenv("R2T_RELEASE_URL_WIN"):
+    logging.info(
+        f"Using R2T_RELEASE_URL_WIN: {os.getenv('R2T_RELEASE_URL_WIN')} for Windows"
+    )
+    url = os.getenv("R2T_RELEASE_URL_WIN").rstrip("/")
+    download_file(url)
+
+if all(os.path.exists(f) for f in DISTRIBUTION_NAMES):
+    exit(0)
+
+
 BASE_URL = "https://artifactory.vi.vector.int/artifactory"
-PATH = "rds-build-packages-generic-dev/code2reqs2tests/distributions/demo_release"
+BRANCH = os.getenv("R2T_RELEASE_BRANCH", "demo_release")
+logging.info(f"Using R2T_RELEASE_BRANCH: {BRANCH}")
+PATH = f"rds-build-packages-generic-dev/code2reqs2tests/distributions/{BRANCH}"
 API_STORAGE_URL = f"{BASE_URL}/api/storage/{PATH}"
-DISTRIBUTION_NAMES = ('autoreq-linux.tar.gz', 'autoreq-win.tar.gz')
 
 with tempfile.TemporaryDirectory() as tmpdirname:
-    os.system(f"wget {API_STORAGE_URL} -O {tmpdirname}/tmp.json > /dev/null 2>&1")
-    with open(f"{tmpdirname}/tmp.json") as f:
+    tmp = Path(tmpdirname, "tmp.json")
+    download_file(API_STORAGE_URL, str(tmp))
+    with open(tmp) as f:
         data = json.load(f)
 
     children_urls = sorted(
@@ -25,20 +62,28 @@ with tempfile.TemporaryDirectory() as tmpdirname:
             if os.path.exists(distribution_name):
                 continue
             url = f"{BASE_URL}/{PATH}{c}/{distribution_name}"
-            status_file = f"{tmpdirname}/status.txt"
-            os.system(
-                f'wget --spider -S {url} 2>&1 | grep "HTTP/"'
-                + " | awk '{print $2}' > "
-                + status_file
-            )
+            status_file = Path(tmpdirname, "status.txt")
+            if platform.system() == "Windows":
+                cmd = (
+                    f"$req = [System.Net.WebRequest]::Create('{url}'); $req.Method = 'HEAD'; "
+                    f"$res = $req.GetResponse(); $status = $res.StatusCode; $res.Close(); "
+                    f"Write-Output $status | Out-File -FilePath '{status_file}' -Encoding UTF8"
+                )
+                cmd = f'powershell -Command "{cmd}"'
+            else:
+                cmd = (
+                    f'wget --spider -S {url} 2>&1 | grep "HTTP/" '
+                    f"| awk '{{print $2}}' > {status_file}"
+                )
+            os.system(cmd)
             with open(status_file) as f:
                 status = f.read().strip()
-            if int(status) == 200:
-                os.system(f"wget {url} > /dev/null 2>&1")
+            if status == "200" or status.strip().endswith("OK"):
+                download_file(url)
 
         if all(os.path.exists(f) for f in DISTRIBUTION_NAMES):
             break
 
-    if not all(os.path.exists(f) for f in DISTRIBUTION_NAMES):
-        logging.error("Failed to download one or more files.")
-        exit(1)
+if not all(os.path.exists(f) for f in DISTRIBUTION_NAMES):
+    logging.error("Failed to download one or more files.")
+    exit(1)
