eae7c49d792959f3a5365e6c14aae6613a660332
Denis Moslavac <Denis.Moslavac@vector.com>
2025-06-26 14:39:57 +0200

    Added requirements tests for manage

--- tests/internal/e2e/test/specs_config.ts
+++ tests/internal/e2e/test/specs_config.ts
@@ -166,7 +166,10 @@ export function getSpecGroups(useVcast24: boolean) {
       params: {},
     };
     specGroups["manage"] = {
-      specs: ["./**/**/vcast_manage.test.ts"],
+      specs: [
+        "./**/**/vcast_manage.test.ts",
+        "./**/**/vcast_requirements_manage.test.ts",
+      ],
       env: { VCAST_USE_PYTHON: "True", MANAGE_TEST: "True" },
       params: {},
     };
@@ -283,7 +286,10 @@ export function getSpecGroups(useVcast24: boolean) {
     };
 
     specGroups["manage_server"] = {
-      specs: ["./**/**/vcast_manage.test.ts"],
+      specs: [
+        "./**/**/vcast_manage.test.ts",
+        "./**/**/vcast_requirements_manage.test.ts",
+      ],
       env: { MANAGE_TEST: "True" },
       params: {},
     };
