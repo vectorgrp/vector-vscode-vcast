477b543408dd450aed98041c39167fd563d478d3
Denis Moslavac <Denis.Moslavac@vector.com>
2025-07-31 13:47:22 +0200

    Adapting tests 6)

--- tests/internal/e2e/test/specs/vcast_build_env_failure_different_paths.test.ts
+++ tests/internal/e2e/test/specs/vcast_build_env_failure_different_paths.test.ts
@@ -202,7 +202,7 @@ async function expectEnvResults(release: string) {
 
   // Iterate thorugh map, expand and check based on release what ENV should be defined.
   for (const entry of release23Value) {
-    const envResult = await findTreeNodeAtLevel(3, `${entry.env}`);
+    const envResult = await findTreeNodeAtLevel(0, `${entry.env}`);
     console.log(`Trying to find ${entry.env} in Tree`);
     if (entry.state === "defined") {
       expect(envResult).not.toBe(undefined);
